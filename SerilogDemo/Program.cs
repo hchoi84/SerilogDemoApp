2466046f8d900862f72b18de5d5d68afc25fe90b
Howard Choi <hchoi84@gmail.com>
2022-08-11 11:55:55 -0700

    Added Serilog

--- SerilogDemo/Program.cs
+++ SerilogDemo/Program.cs
@@ -1,40 +1,78 @@
 //https://www.youtube.com/watch?v=oXNslgIXIbQ&ab_channel=IAmTimCorey
+using Microsoft.Extensions.Logging.Configuration;
+using Serilog;
 using SerilogDemo.Data;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Host.ConfigureLogging((context, logging) =>
+#region Serilog
+var configuraiton = new ConfigurationBuilder()
+  .AddJsonFile("appsettings.json")
+  .Build();
+
+Log.Logger = new LoggerConfiguration()
+  .ReadFrom.Configuration(configuraiton)
+  .CreateLogger();
+
+Log.Information("================================");
+Log.Information("App starting up at {datetime} UTC", DateTime.UtcNow);
+#endregion
+
+try
 {
-  logging.ClearProviders();
-  logging.AddConfiguration(context.Configuration.GetSection("Logging"));
-  //logging.AddDebug();
-  logging.AddConsole();
-  //EventSource, EventLog, TraceSource, AzureAppServicesFile, AzureAppServicesBlob, ApplicationInsights
-});
+  var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+  #region Serilog
+  builder.Host.UseSerilog();
+  #endregion
 
-var app = builder.Build();
-var logger = app.Services.GetRequiredService<ILogger<Program>>();
-logger.LogInformation("App started {datetime} UTC", DateTime.UtcNow);
+  #region Default Logging Stuff
+  //builder.Host.ConfigureLogging((context, logging) =>
+  //{
+  //  logging.ClearProviders();
+  //  logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+  //  //logging.AddDebug();
+  //  logging.AddConsole();
+  //  //EventSource, EventLog, TraceSource, AzureAppServicesFile, AzureAppServicesBlob, ApplicationInsights
+  //});
+  #endregion
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-  app.UseExceptionHandler("/Error");
-  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-  app.UseHsts();
+  // Add services to the container.
+  builder.Services.AddRazorPages();
+  builder.Services.AddServerSideBlazor();
+  builder.Services.AddSingleton<WeatherForecastService>();
+
+  var app = builder.Build();
+
+  #region Default Logging Stuff
+  //var logger = app.Services.GetRequiredService<ILogger<Program>>();
+  //logger.LogInformation("App started {datetime} UTC", DateTime.UtcNow);
+  #endregion
+
+  // Configure the HTTP request pipeline.
+  if (!app.Environment.IsDevelopment())
+  {
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+  }
+
+  app.UseHttpsRedirection();
+
+  app.UseStaticFiles();
+
+  app.UseSerilogRequestLogging();
+
+  app.UseRouting();
+
+  app.MapBlazorHub();
+  app.MapFallbackToPage("/_Host");
+
+  app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+catch (Exception ex)
+{
+  Log.Fatal(ex, "The app failed to start");
+}
+finally
+{
+  Log.CloseAndFlush();
+}
